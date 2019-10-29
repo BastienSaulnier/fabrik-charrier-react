60054ca8becd1d96b9dd2b3d8a6b74d2f4abaaa9
Bastien Saulnier <bastien.saul@gmail.com>
2019-10-29 16:56:48 +0100

    Import of all Components to APP + Initialisation of Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import { Switch, Route } from "react-router-dom";
 import "./App.scss";
 
 class App extends React.Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <NavBar />
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/products" component={Products} />
+          <Route path="/portfolio" component={Portfolio} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+
+        <Footer />
+      </div>
+    );
   }
 }
 
