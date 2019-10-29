14e93d9007abc2b1d42262d2b74736ef6a0e8e81
Bastien Saulnier <bastien.saul@gmail.com>
2019-10-29 16:55:04 +0100

    Footer Component creation + Footer CSS

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./Footer.scss";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="Footer">
+        <div className="HeadFoot">
+          <ul className="FootAdress">
+            <li>
+              <i className="fas fa-user"></i>
+              <p>Fabrik-Charrier</p>
+            </li>
+            <li>
+              <i className="fas fa-map-marker-alt"></i>
+              <p>
+                5189 Chaussée Brenehaut
+                <br />
+                622040 Longfossé
+              </p>
+            </li>
+            <li>
+              <i className="fas fa-phone"></i>
+              <a href="tel:+33677900823">06 77 90 08 23</a>
+            </li>
+            <li>
+              <i className="fas fa-at"></i>
+              <a href="mailto:lafabrik.ebeniste@gmail.com">
+                lafabrik.ebeniste@gmail.com
+              </a>
+            </li>
+          </ul>
+          <div className="FootSocials">
+            <strong>à Propos</strong>
+            <hr />
+            <div className="FootInfos">
+              <p>
+                La Fabrik-Charrier est une SARL fondée en 2018 par Guillaume
+                Charrier. Forte de ses XX ans d'expérience, et passionés par
+                l'artisanat, nous sommes prêts à répondre à toutes vos demandes
+                et vous acompagner dans l'élaboration de votre projet.
+              </p>
+              <ul className="Socials">
+                <li>
+                  <a
+                    href="https://www.facebook.com/la.fabrik.charrier"
+                    target="_blank"
+                    id="fb"
+                  >
+                    <i className="fab fa-facebook-square"></i>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.instagram.com/fabrikcharrier/?hl=fr"
+                    target="_blank"
+                    id="inst"
+                  >
+                    <i className="fab fa-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" target="_blank" id="gmail">
+                    <i className="fas fa-envelope-square"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="Foot">
+          <p>
+            <strong>
+              <a href="https://lafabrikcharrier.com">lafabrikcharrier.com </a>
+            </strong>{" "}
+            made with <i className="fas fa-heart"></i> by{" "}
+            <strong>Bastien Saulnier</strong>, Copyright 2019.
+          </p>
+        </div>
+      </footer>
+    );
+  }
+}
+export default Footer;
