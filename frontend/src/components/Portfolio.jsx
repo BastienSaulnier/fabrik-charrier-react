05b4e0b929b7a4ad4ec19d701c94e7a670383550
Bastien Saulnier <bastien.saul@gmail.com>
2019-10-29 16:55:42 +0100

    Portfolio Component declaration + Portfolio CSS

---
+++ frontend/src/components/Portfolio.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Portfolio.scss";
+
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <div className="Container">
+        <div className="Hero-portfolio overlay-2">
+          <div className="center">
+            <div className="Title">
+              <p>
+                <span>P</span>
+                <span>O</span>
+                <span>R</span>
+                <span>T</span>
+                <span>F</span>
+                <span>O</span>
+                <span>L</span>
+                <span>I</span>
+                <span>O</span>
+              </p>
+              <strong>
+                <a href="index.php">lafabrikcharrier.com</a>
+              </strong>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
