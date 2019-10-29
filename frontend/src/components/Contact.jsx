790cedc3921a8f580b838770470da1f57e103536
Bastien Saulnier <bastien.saul@gmail.com>
2019-10-29 16:55:58 +0100

    Contact Component declaration + Contact CSS

---
+++ frontend/src/components/Contact.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Contact.scss";
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <div className="Container">
+        <div className="Hero-contact overlay-2">
+          <div className="center">
+            <div className="Title">
+              <p>
+                <span>C</span>
+                <span>O</span>
+                <span>N</span>
+                <span>T</span>
+                <span>A</span>
+                <span>C</span>
+                <span>T</span>
+              </p>
+              <strong>
+                <a href="index.php">lafabrikcharrier.com</a>
+              </strong>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contact;
