35dd57bd34e70b4c305ebd66e45322df42fa0eed
Saulnier <bastien.saul@gmail.com>
2019-11-21 16:11:18 +0100

    Updated Products component to be function + css + dummy infos for product example

--- frontend/src/components/ProductsList.js
+++ frontend/src/components/ProductsList.js
@@ -1,35 +1,119 @@
 let ProductsList = [
   {
+    productId: 1,
     photo: "/img/slider-produits/Buffet.jpg",
     product: "Buffet Design",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
     price: 250,
     category: "Buffets",
-    note: "Blablabla"
+    note: "Blablabla",
+    lightboxGallery: [
+      {
+        src: "/img/slider-produits/Buffet.jpg",
+        title: "Buffet-1",
+        description: "Buffet-1 description"
+      },
+      {
+        src: "/img/slider-produits/Buffet.jpg",
+        title: "Buffet-1",
+        description: "Buffet-1 description"
+      },
+      {
+        src: "/img/slider-produits/Buffet.jpg",
+        title: "Buffet-1",
+        description: "Buffet-1 description"
+      },
+      {
+        src: "/img/slider-produits/Buffet.jpg",
+        title: "Buffet-1",
+        description: "Buffet-1 description"
+      }
+    ]
   },
   {
+    productId: 2,
     photo: "/img/slider-produits/Table.jpg",
     product: "Table Design",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
     price: 300,
     category: "Tables",
-    note: "Blublublu"
+    note: "Blublublu",
+    lightboxGallery: [
+      {
+        src: "/img/slider-produits/Table.jpg"
+      },
+      {
+        src: "/img/slider-produits/Table.jpg"
+      },
+      {
+        src: "/img/slider-produits/Table.jpg"
+      },
+      {
+        src: "/img/slider-produits/Table.jpg"
+      }
+    ]
   },
   {
+    productId: 3,
     photo: "/img/slider-produits/Table-Basse.jpg",
     product: "Table-Basse Design",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
     price: 170,
     category: "Tables Basses",
-    note: "Blebleble"
+    note: "Blebleble",
+    lightboxGallery: [
+      {
+        src: "/img/slider-produits/Table-Basse.jpg",
+        title: "Table-Basse-1",
+        description: "Table-Basse-1 description"
+      },
+      {
+        src: "/img/slider-produits/Table-Basse.jpg",
+        title: "Table-Basse-1",
+        description: "Table-Basse-1 description"
+      },
+      {
+        src: "/img/slider-produits/Table-Basse.jpg",
+        title: "Table-Basse-1",
+        description: "Table-Basse-1 description"
+      },
+      {
+        src: "/img/slider-produits/Table-Basse.jpg",
+        title: "Table-Basse-1",
+        description: "Table-Basse-1 description"
+      }
+    ]
   },
   {
+    productId: 4,
     photo: "/img/slider-produits/Secretaire.jpg",
     product: "Secretaire Design",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
     price: 600,
     category: "Secretaires",
-    note: "Bliblibli"
+    note: "Bliblibli",
+    lightboxGallery: [
+      {
+        src: "/img/slider-produits/Secretaire.jpg",
+        title: "Secretaire-1",
+        description: "Secretaire-1 description"
+      },
+      {
+        src: "/img/slider-produits/Secretaire.jpg",
+        title: "Secretaire-1",
+        description: "Secretaire-1 description"
+      },
+      {
+        src: "/img/slider-produits/Secretaire.jpg",
+        title: "Secretaire-1",
+        description: "Secretaire-1 description"
+      },
+      {
+        src: "/img/slider-produits/Secretaire.jpg",
+        title: "Secretaire-1",
+        description: "Secretaire-1 description"
+      }
+    ]
   }
 ];
 
