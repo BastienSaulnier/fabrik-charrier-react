9694318c580488d39c3dda81efee41696152593c
Saulnier <bastien.saul@gmail.com>
2019-12-13 17:24:37 +0100

    Made functional component + axios.get

--- frontend/src/components/ReactLightbox.jsx
+++ frontend/src/components/ReactLightbox.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Lightbox from "react-lightbox-component";
-import PortfolioPhotosList from "./PortfolioPhotosList.js";
+import axios from "axios";
 import "../../node_modules/react-lightbox-component/build/css/index.css";
 
-class ReactLightbox extends React.Component {
-  render() {
-    return (
-      <div className="Lightbox">
+function ReactLightbox() {
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5050/portfolio")
+      .then(res => {
+        setPhotos(res.data);
+        setLoading(false);
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, [setPhotos]);
+
+  return (
+    <div className="Lightbox">
+      {loading && <p className="loader">... Loading ...</p>}
+      {!loading && (
         <Lightbox
-          images={PortfolioPhotosList}
+          images={photos}
           thumbnailWidth="calc(25% - 10px)"
           thumbnailHeight="150px"
         />
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
 
 export default ReactLightbox;
