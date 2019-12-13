10afeaa047cbf1f7d4de59e211d1547fe68307df
Saulnier <bastien.saul@gmail.com>
2019-12-13 13:39:41 +0100

    Made get route for portfolio

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,17 @@ app.use(
   })
 );
 
+app.get("/portfolio", (req, res) => {
+  db.query(`SELECT * FROM portfolio`, (err, results) => {
+    if (err) {
+      console.log("Error on GET /portfolio!");
+      console.error(err);
+      return res.status(500).send("Sorry, we encountered an internal error.");
+    }
+    return res.status(200).json(results);
+  });
+});
+
 app.listen(serverPort, err => {
   if (err) throw err;
   console.log(`Server listening on http://localhost:${serverPort}`);
